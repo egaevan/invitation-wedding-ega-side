d76fa099328f2b91d3f81714e0c6a889a7b0ae78
egaevan <ega.evan@salt.co.id>
2022-12-17 13:34:24 +0700

    Merge Dev

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -38,11 +38,11 @@ export default function Page() {
           <div className={classnames('w-[350px] h-[418px] relative mt-[1em]')}>
             <Image src={Flower} object-fit="fill" alt="flower-image" ref={item2 => { imageFlower = item2 }} />
             <div className={classnames('absolute flex flex-col top-[150px] left-0 right-0 ml-auto mr-auto text-center opacity-0')} ref={item2 => { containerName = item2 }}>
-              <h1 className={classnames('text-title text-[40px] leading-[150%] tracking-[0.3em]')}>ICA</h1>
+              <h1 className={classnames('text-title text-[40px] leading-[150%] tracking-[0.3em]')}>EGA</h1>
               <div className={classnames('w-[39px] h-[43px] text-center flex items-center justify-center ml-auto mr-auto')}>
                 <Image src={Ring} object-fit='fill' alt="ring-image" />
               </div>
-              <h1 className={classnames('text-title text-[40px] leading-[150%] tracking-[0.3em]')}>EGA</h1>
+              <h1 className={classnames('text-title text-[40px] leading-[150%] tracking-[0.3em]')}>ICA</h1>
             </div>
           </div>
           <button className={classnames('py-[14px] min-w-[350px] mt-[82px] rounded-md relative bg-[#D49494] text-white opacity-0 hover:bg-[#e8aeae]')} onClick={() => { btnOpen() }} ref={item3 => { btnWedding = item3 }}>
